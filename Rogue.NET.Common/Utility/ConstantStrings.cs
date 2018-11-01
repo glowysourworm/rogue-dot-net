f24981acffb8d542deb19cba778f3166737c347a
Ryan <ryanddolan@hotmail.com>
2018-11-01 13:12:23 -0500

    Moved copyright string to updating static resource

---
+++ Rogue.NET.Common/Utility/ConstantStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue.NET.Common.Utility
+{
+    public static class ConstantStrings
+    {
+        public static string Copyright = string.Format("© {0} - Rogue.NET Project", DateTime.Now.Year);
+    }
+}
