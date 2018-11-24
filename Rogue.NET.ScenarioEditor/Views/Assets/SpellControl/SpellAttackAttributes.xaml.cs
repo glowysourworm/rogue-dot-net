7723688cc16966aa2ba5f8ac7a15c82a4f871872
Ryan <ryanddolan@hotmail.com>
2018-11-23 22:24:18 -0600

    Completed spell editor

---
+++ Rogue.NET.ScenarioEditor/Views/Assets/SpellControl/SpellAttackAttributes.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rogue.NET.ScenarioEditor.Views.Assets.SpellControl
+{
+    /// <summary>
+    /// Interaction logic for SpellAttackAttributes.xaml
+    /// </summary>
+    public partial class SpellAttackAttributes : UserControl
+    {
+        public SpellAttackAttributes()
+        {
+            InitializeComponent();
+        }
+    }
+}
