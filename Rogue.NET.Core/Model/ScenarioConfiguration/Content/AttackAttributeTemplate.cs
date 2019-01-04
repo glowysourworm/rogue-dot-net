7cd6f2e8a60f22f547f99b271c3b97d2fecced29
Ryan <ryanddolan@hotmail.com>
2019-01-04 12:17:30 -0600

    Scenario Configuration:  Added back attack attribute weakness

--- Rogue.NET.Core/Model/ScenarioConfiguration/Content/AttackAttributeTemplate.cs
+++ Rogue.NET.Core/Model/ScenarioConfiguration/Content/AttackAttributeTemplate.cs
@@ -8,6 +8,7 @@ namespace Rogue.NET.Core.Model.ScenarioConfiguration.Content
     {
         private Range<double> _attack;
         private Range<double> _resistance;
+        private Range<int> _weakness;
 
         public Range<double> Attack
         {
@@ -33,11 +34,24 @@ namespace Rogue.NET.Core.Model.ScenarioConfiguration.Content
                 }
             }
         }
+        public Range<int> Weakness
+        {
+            get { return _weakness; }
+            set
+            {
+                if (_weakness != value)
+                {
+                    _weakness = value;
+                    OnPropertyChanged("Weakness");
+                }
+            }
+        }
 
         public AttackAttributeTemplate()
         {
             this.Attack = new Range<double>(0, 0, 0, 5000);
             this.Resistance = new Range<double>(0, 0, 0, 5000);
+            this.Weakness = new Range<int>(0, 0, 0, 10);
         }
     }
 }
--- Rogue.NET.ScenarioEditor/ViewModel/ScenarioConfiguration/Content/AttackAttributeTemplateViewModel.cs
+++ Rogue.NET.ScenarioEditor/ViewModel/ScenarioConfiguration/Content/AttackAttributeTemplateViewModel.cs
@@ -6,6 +6,7 @@ namespace Rogue.NET.ScenarioEditor.ViewModel.ScenarioConfiguration.Content
     {
         private RangeViewModel<double> _attack;
         private RangeViewModel<double> _resistance;
+        private RangeViewModel<int> _weakness;
 
         public RangeViewModel<double> Attack
         {
@@ -17,11 +18,17 @@ namespace Rogue.NET.ScenarioEditor.ViewModel.ScenarioConfiguration.Content
             get { return _resistance; }
             set { this.RaiseAndSetIfChanged(ref _resistance, value); }
         }
+        public RangeViewModel<int> Weakness
+        {
+            get { return _weakness; }
+            set { this.RaiseAndSetIfChanged(ref _weakness, value); }
+        }
 
         public AttackAttributeTemplateViewModel()
         {
             this.Attack = new RangeViewModel<double>(0, 0, 0, 5000);
             this.Resistance = new RangeViewModel<double>(0, 0, 0, 5000);
+            this.Weakness = new RangeViewModel<int>(0, 0, 0, 10);
         }
     }
 }
