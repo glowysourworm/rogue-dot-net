f0a6abf0e481fe2246f90907cc793de60b6ead72
Ryan <ryanddolan@hotmail.com>
2019-08-17 17:47:46 -0500

    *** Alteration Refactoring
    Created scenario meta-data common control. mostly cosmetic changes

---
+++ Rogue.NET.ScenarioEditor/Views/Assets/SharedControl/MetaDataControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Rogue.NET.ScenarioEditor.Views.Assets.SharedControl
+{
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [Export]
+    public partial class MetaDataControl : UserControl
+    {
+        public static readonly DependencyProperty HasUniqueSettingProperty =
+            DependencyProperty.Register("HasUniqueSetting", typeof(bool), typeof(MetaDataControl), new PropertyMetadata(true));
+
+        public static readonly DependencyProperty HasCursedSettingProperty =
+            DependencyProperty.Register("HasCursedSetting", typeof(bool), typeof(MetaDataControl), new PropertyMetadata(true));
+
+        public bool HasUniqueSetting
+        {
+            get { return (bool)GetValue(HasUniqueSettingProperty); }
+            set { SetValue(HasUniqueSettingProperty, value); }
+        }
+
+        public bool HasCursedSetting
+        {
+            get { return (bool)GetValue(HasCursedSettingProperty); }
+            set { SetValue(HasCursedSettingProperty, value); }
+        }
+
+        public MetaDataControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
