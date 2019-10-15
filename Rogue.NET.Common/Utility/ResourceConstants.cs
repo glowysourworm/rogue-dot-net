3ca4432ca72df0b76575d9e37d5676e0db124e42
rdolan <ryanddolan@hotmail.com>
2019-10-15 02:21:10 -0500

    Moved save / scenarios folders to the root solution folder
    (also removed some old files)

--- Rogue.NET.Common/Utility/ResourceConstants.cs
+++ Rogue.NET.Common/Utility/ResourceConstants.cs
@@ -8,8 +8,8 @@ namespace Rogue.NET.Common.Utility
 {
     public static class ResourceConstants
     {
-        public const string SavedGameDirectory = "..\\save";
-        public const string ScenarioDirectory = "..\\scenarios";
+        public const string SavedGameDirectory = "..\\..\\..\\save";
+        public const string ScenarioDirectory = "..\\..\\..\\scenarios";
         public const string ScenarioExtension = "rdn";
         public const string ScenarioConfigurationExtension = "rdns";
         public const string EmbeddedScenarioDirectory = "..\\..\\..\\Rogue.NET.Common\\Resource\\Configuration";
